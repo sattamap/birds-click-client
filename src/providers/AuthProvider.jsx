5cab845d37e6019a895625c8a60b6a5de678bc3b
sattamap <sattamap@gmail.com>
2023-12-27 16:51:03 +0600

    Add signIn and logOut functions to handle user authentication

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,23 +1,39 @@
 import { createContext,  useState } from "react";
-
+import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { app } from "../firebase/firebase.config";
 
 
 export const AuthContext = createContext(null);
 
 
-
+const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-   
+    const signIn = (email,password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth,email,password);
+    }
+
+
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
     
     const authInfo = {
         user,
         setUser,
+        signIn,
+        logOut,
+        loading,
        
     };
+
+ 
+
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
