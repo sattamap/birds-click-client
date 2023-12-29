1fe66f57fc173069c7b612cf43e9542e9fb931e6
sattamap <sattamap@gmail.com>
2023-12-29 18:19:52 +0600

    Create useAuth hook and  Enhance Dashboard component with user authentication and navigation

--- src/layout/Dashboard.jsx
+++ src/layout/Dashboard.jsx
@@ -1,8 +1,33 @@
+import { NavLink, Outlet } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const Dashboard = () => {
+    const { user } = useAuth();
     return (
-        <div>
-            <h2>This is my dashboard</h2>
+        <div className="flex">
+            <div className="w-64 min-h-screen bg-[#38a9a1]">
+                <div className="flex flex-col items-center justify-center p-4">
+                    <img src={user?.photoURL} alt="Profile" className="w-20 h-20 rounded-full mr-2" />
+                    <span className="text-white">{user?.displayName}</span>
+                </div>
+                <ul className="menu p-4">
+                    <li><NavLink to="/dashboard/addTest">
+                        Add Test
+                    </NavLink>
+                    </li>
+
+                    <div className="divider"></div>
+                    <li>
+                        <NavLink to="/">
+                            Home
+                        </NavLink>
+                    </li>
+
+                </ul>
+            </div>
+            <div className="flex-1 p-10">
+                <Outlet></Outlet>
+            </div>
         </div>
     );
 };
