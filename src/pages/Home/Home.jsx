91fa8da5ba95cde1d27115ef5c5ba1711a6f8f68
sattamap <sattamap@gmail.com>
2024-01-05 17:44:43 +0600

    Add Footer component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 
 import Banner from "../../components/Banner/Banner";
 import Feature from "../../components/Feature/Feature";
+import Footer from "../../components/Footer/Footer";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Feature></Feature>
+            <Footer></Footer>
         </div>
     );
 };
