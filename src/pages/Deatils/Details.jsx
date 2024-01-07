858a6a51ace29422414350fecf16b3195c77ab69
sattamap <sattamap@gmail.com>
2024-01-07 23:18:27 +0600

    Make Details component responsive for mobile devices

--- src/pages/Deatils/Details.jsx
+++ src/pages/Deatils/Details.jsx
@@ -2,39 +2,49 @@ import { useParams } from "react-router-dom";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
 import { useEffect, useState } from "react";
 
-
 const Details = () => {
+  const { id } = useParams();
+  const axiosPublic = useAxiosPublic();
+  const [birds, setBirds] = useState([]);
 
-    const { id } = useParams();
-    const axiosPublic = useAxiosPublic();
-    const [birds, setBirds] = useState([]);
+  useEffect(() => {
+    axiosPublic.get('/birds').then((response) => {
+      setBirds(response.data);
+    });
+  }, [axiosPublic]);
 
-    useEffect(() => {
-        axiosPublic.get('/birds').then((response) => {
-            setBirds(response.data);
-        });
-      }, [axiosPublic]);
-    
-      const birdDetails = birds.find((bird) => bird._id === id);
-    return (
-        <div>
-          <div className="hero min-h-screen bg-base-200">
-  <div className="hero-content flex-col lg:flex-row gap-14">
-    <div className="">
-    <img src={birdDetails?.image} className=" rounded-lg shadow-2xl" />
-    </div>
-    <div className="lg:w-2/5 ">
-      <h1 className="text-base font-bold" >Name of the Bird <span className="ml-[14px]">:</span><span className="text-xl font-bold"> {birdDetails?.birdNameENG} ({birdDetails?.birdNameBD})</span> </h1>
-      <h1 className="text-base font-bold " > Location <span className="ml-20">:</span>  <span className="text-xl font-bold">{birdDetails?.location} </span> </h1>
-      <h1 className="text-base font-bold" > Date <span className="ml-[109px]">:</span> <span className="text-xl font-bold">{birdDetails?.date} </span> </h1>
-      <h1 className="text-base font-bold" > Camera Setup <span className="ml-[40px]">:</span> <span className="text-xl font-bold">{birdDetails?.camera} </span> </h1>
-      <p className="text-justify overflow-auto max-h-64 mt-16">{birdDetails?.description}</p>
+  const birdDetails = birds.find((bird) => bird._id === id);
 
-    </div>
-  </div>
-</div>
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col gap-6">
+          <div className="lg:flex lg:gap-8">
+            <img src={birdDetails?.image} className="w-full rounded-lg shadow-2xl" alt={birdDetails?.birdNameENG} />
+            <div className="lg:w-2/3 mt-6 lg:mt-0">
+              <h1 className="text-base font-medium mb-2 lg:mb-4">
+                Name of the Bird <span className="lg:mr-2">:</span>
+                <span className="text-lg font-bold block xl:inline ">{birdDetails?.birdNameENG} ({birdDetails?.birdNameBD})</span>
+              </h1>
+              <h1 className="text-base font-medium mb-2 lg:mb-4">
+                Location <span className="lg:ml-16 lg:mr-2">:</span>
+                <span className="text-lg font-bold block xl:inline">{birdDetails?.location}</span>
+              </h1>
+              <h1 className="text-base font-medium mb-2 lg:mb-4">
+                Date <span className="lg:ml-24 lg:mr-2">:</span>
+                <span className="text-lg font-bold block xl:inline">{birdDetails?.date}</span>
+              </h1>
+              <h1 className="text-base font-medium mb-2 lg:mb-4">
+                Camera Setup <span className="lg:ml-4 lg:mr-2">:</span>
+                <span className="text-lg font-bold block xl:inline">{birdDetails?.camera}</span>
+              </h1>
+              <p className="text-justify overflow-auto max-h-64">{birdDetails?.description}</p>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Details;
